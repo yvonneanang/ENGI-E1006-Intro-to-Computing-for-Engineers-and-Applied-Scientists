3ca9262abff762c8e78edbb66944590caf822d08
Yvonne Anang <30832316+yvonneanang@users.noreply.github.com>
2026-04-04 19:36:31 -0400

    In-class set 0

---
+++ project0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#"""
+#Created on Mon Sep 16 15:39:34 2019
+#
+#@author: Yvonne
+#"""
+
+number_of_wives = 7
+number_of_sacks = 7 * number_of_wives
+number_of_cats = 7 * number_of_sacks
+number_of_kittens = 7 * number_of_cats
+
+print (number_of_kittens)
