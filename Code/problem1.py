1871502db4f3208c97e8cad45cea1a4813c72bf8
Yvonne Anang <30832316+yvonneanang@users.noreply.github.com>
2026-04-04 20:19:21 -0400

    in class set 1 code

---
+++ Code/problem1.py
@@ -0,0 +1,16 @@
+#submitted by Yvonne Naa Ardua Anang (uni: yna2103)
+# completed by Mikaela Zhang(uni:xz2782) and Yvonne Naa Ardua Anang (uni:yna2103)
+
+# problem 1
+
+height_in_cm = int(input ("Please type height in centimeters:"))
+width_in_cm = int(input ("Please type width in centimeters:"))
+length_in_cm = int(input ("Please type length in centimeters:"))
+
+
+area_to_be_painted_cm_squared = (2 * width_in_cm*height_in_cm) + (2 * length_in_cm * height_in_cm) + (length_in_cm * width_in_cm)
+area_to_be_painted_m_squared = area_to_be_painted_cm_squared/10000
+paint_in_liters = area_to_be_painted_m_squared/10 * 2.5
+print ('The volume of paint needed is ',(paint_in_liters), 'liters')
+
+
