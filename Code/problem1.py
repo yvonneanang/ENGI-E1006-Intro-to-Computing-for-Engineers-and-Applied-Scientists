828195fc2df6eb6bffbd3de7a9fed0d95407f889
Yvonne Anang <30832316+yvonneanang@users.noreply.github.com>
2026-04-04 20:26:57 -0400

    in class set 2 - problem 1

---
+++ Code/problem1.py
@@ -0,0 +1,35 @@
+#submitted by Yvonne Naa Ardua Anang (uni:yna2103)
+#collaborated with Joseph Duodu(uni: jd3519)
+
+#part 1
+def piggify(word):
+    vowels ="aeiou"
+    if word[0] in vowels:
+        piggified = word + "yay"
+    
+    elif word[0] not in vowels:
+        for letter in word:
+            if letter in vowels:
+                index_of_letter = word.find(letter)
+                spliced_consonant = (word[:index_of_letter])
+                spliced_vowel = (word[index_of_letter:])
+                piggified = spliced_vowel + spliced_consonant + "ay"
+                break
+            if letter not in vowels:
+                piggified = word + "ay"
+    return piggified
+
+#part 2
+while True:
+    word = input("Please enter a word:>>>")
+    if word != ".":
+        answer = piggify(word)
+        print (answer)
+
+    if word == ".":
+        break
+    
+
+
+
+    
